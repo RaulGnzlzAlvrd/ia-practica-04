5e1a03eec20a8681fccf7058414509b402276507
Raúl González <gonzalv@ciencias.unam.mx>
2020-02-26 16:20:06 -0600

    Inicializa la clase RecocidoSimulado con los datos de un archivo tsp

--- recocido/Main.java
+++ recocido/Main.java
@@ -1,16 +1,23 @@
-/**
-Clase para ejecutar un proceso de optimizacion usando recocido simulado
-@author Benjamin Torres Saavedra
-@version 0.1
-*/
 package recocido;
-import static java.lang.System.out;    
+
+import static java.lang.System.out;   
+import java.io.IOException; 
+
+/**
+ * Clase para ejecutar un proceso de optimizacion usando recocido simulado
+ * @author Benjamin Torres Saavedra
+ * @version 0.1
+ */
 public class Main{
-   public static void main(String []args){
-        int interaciones = 100; // Número de iteraciones que se van a hacer.
-        RecocidoSimulado recocido = new RecocidoSimulado();
-        Solucion s = recocido.ejecutar();
-        out.println(s);
-      
-   }
+   	public static void main(String []args) {
+    	int interaciones = 100; // Número de iteraciones que se van a hacer.
+        String filePath = "recocido/Djibouti.tsp"; // Nombre del archivo tsp con la lista de coordenadas de ciudades.
+        try {
+        	RecocidoSimulado recocido = new RecocidoSimulado(filePath);
+        	Solucion s = recocido.ejecutar();
+        	out.println(s);
+        } catch (IOException ioe) {
+        	out.println("No se pudo leer el archivo.\n" + ioe);
+        }
+   	}
 }
--- recocido/RecocidoSimulado.java
+++ recocido/RecocidoSimulado.java
@@ -1,53 +1,89 @@
-/**
-Interface con los metodos necesarios para implementar el metodo
-de recocido simulado junto con la solucion a un problema particular.
-
-@author Benjamin Torres
-@version 0.1
-*/
 package recocido;
 
+import java.io.*;
+import java.util.LinkedList;
+
+/**
+ * Interface con los metodos necesarios para implementar el metodo
+ * de recocido simulado junto con la solucion a un problema particular.
+ *
+ * @author Benjamin Torres
+ * @version 0.1
+ */
 public class RecocidoSimulado{
   float temperatura;
   float valor;
   float decaimiento;
+  LinkedList<Ciudad> listaCiudades = new LinkedList<>();
+  String filePath;
 
   /**
-  Inicializa los valores necesarios para realizar 
-  recocido simulado durante un numero determinado de iteraciones
-  */
-  public RecocidoSimulado(){ //escoge los parametros necesarios para inicializar el algoritmo
- 	
+   * Inicializa los valores necesarios para realizar 
+   * recocido simulado durante un numero determinado de iteraciones
+   * @param filePath El nombre del archivo del que va a sacar las coordenadas de las ciudades
+   */
+  public RecocidoSimulado(String filePath) throws IOException {
+    this.filePath = filePath;
+    inicializaLista();
   }
 
   /**
-  Funcion que calcula una nueva temperatura en base a
-  la anterior y el decaemiento usado.
-  @param temperatura, float con el valor actual 
-  @param decaimiento, float que sera usado para hacer decaer el valor de temperatura
-  @return nueva temperatura
-  */
+   * Funcion que calcula una nueva temperatura en base a
+   * la anterior y el decaemiento usado.
+   * @param temperatura, float con el valor actual 
+   * @param decaimiento, float que sera usado para hacer decaer el valor de temperatura
+   * @return nueva temperatura
+   */
   public float nuevaTemperatura(float temperatura,float decaimiento){
     return 0;
   }
 
   /**
-  Genera y devuelve la solucion siguiente dependiendo de su valor
-  y de la probabilidad de elegir una solucion peor
-  @param Solucion que sera usada como base para elegir a la siguiente
-  @return Solucion nueva
-  */
+   * Genera y devuelve la solucion siguiente dependiendo de su valor
+   * y de la probabilidad de elegir una solucion peor
+   * @param Solucion que sera usada como base para elegir a la siguiente
+   * @return Solucion nueva
+   */
   public Solucion seleccionarSiguienteSolucion(Solucion s){
     return new Solucion();
   }
 
   /**
-  Ejecuta el algoritmo con los parametros con los que fue inicializado
-  devuelve una solucion.
-  @param
-  @return Solucion al problema
-  */
+   * Ejecuta el algoritmo con los parametros con los que fue inicializado
+   * devuelve una solucion.
+   * @param
+   * @return Solucion al problema
+   */
   public Solucion ejecutar(){
     return new Solucion();
+  }
+
+  /**
+   * Saca las coordenadas de un archivo de ciudades y la convierte en una lista de Ciudad.
+   * 
+   * Para el formato del archivo se puede visitar la siguiente página:
+   * http://www.math.uwaterloo.ca/tsp/world/dj38.tsp
+   * @param 
+   * @return
+   */
+  private void inicializaLista() throws IOException {
+    File file = new File(filePath);
+    if(!file.exists()) {
+      throw new IOException("Archivo no encontrado.");
+    }
+
+    BufferedReader fileReader = new BufferedReader(new FileReader(file));
+    String row;
+    Ciudad ciudad;
+    while (!(row = fileReader.readLine()).contains("NODE_COORD_SECTION")) {}
+    while ((row = fileReader.readLine()) != null) {
+      String[] data = row.split(" ");
+      double x = Double.parseDouble(data[1]); 
+      double y = Double.parseDouble(data[2]); 
+      int id = Integer.parseInt(data[0]); 
+      ciudad = new Ciudad(x, y, id);
+      listaCiudades.add(ciudad);
+    }
+    fileReader.close();
   }
 }
